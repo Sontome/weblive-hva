284eaa35a0cf75ef069aa9830b1b5c163da641d9
Sontome <93179211+Sontome@users.noreply.github.com>
2025-10-26 09:52:33 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -11,12 +10,22 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  optimizeDeps: {
+    include: ["html2canvas"],
+  },
+  build: {
+    rollupOptions: {
+      external: [], // 👈 thêm dòng này nếu chưa có
+    },
+    commonjsOptions: {
+      include: [/node_modules/, /html2canvas/],
     },
   },
 }));
